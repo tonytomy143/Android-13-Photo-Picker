d73f17b4fe97243e14a967b1f96750b82cb1c508
Tony Tomy <tony.tomy@trenser.com>
2022-12-29 12:22:55 +0530

    image picker logic added

--- Android13PhotoPicker/app/src/main/java/com/example/android13photopicker/MainActivity.kt
+++ Android13PhotoPicker/app/src/main/java/com/example/android13photopicker/MainActivity.kt
@@ -26,5 +26,80 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        exoPlayer = ExoPlayer.Builder(this).build()
+        binding.exoPlayer.player = exoPlayer
+
+        // Initialize single media picker launcher
+        pickSingleMediaLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode != Activity.RESULT_OK) {
+                    Toast.makeText(this, "Failed picking media.", Toast.LENGTH_SHORT).show()
+                } else {
+                    val uri = it.data?.data
+                    showSnackBar("SUCCESS: ${uri?.path}")
+
+                    binding.ivImage.setImageURI(uri)
+                }
+            }
+
+        // Initialize multiple media picker launcher
+        pickMultipleMediaLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode != Activity.RESULT_OK) {
+                    Toast.makeText(this, "Failed picking media.", Toast.LENGTH_SHORT).show()
+                } else {
+                    val uris = it.data?.clipData ?: return@registerForActivityResult
+                    var uriPaths = ""
+                    for (index in 0 until uris.itemCount) {
+                        uriPaths += uris.getItemAt(index).uri.path
+                        uriPaths += "\n"
+                    }
+                    showSnackBar("SUCCESS: $uriPaths")
+                }
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Setup pick 1 image/video
+            binding.buttonPickPhotoVideo.setOnClickListener {
+                pickSingleMediaLauncher.launch(
+                    Intent(MediaStore.ACTION_PICK_IMAGES)
+                )
+            }
+            // Setup pick image
+            binding.buttonPickPhoto.setOnClickListener {
+                pickSingleMediaLauncher.launch(
+                    Intent(MediaStore.ACTION_PICK_IMAGES)
+                        .apply {
+                            type = "image/*"
+                        }
+                )
+            }
+            // Setup pick video
+            binding.buttonPickVideo.setOnClickListener {
+                pickSingleMediaLauncher.launch(
+                    Intent(MediaStore.ACTION_PICK_IMAGES)
+                        .apply {
+                            type = "video/*"
+                        }
+                )
+            }
+        } else {
+            Toast.makeText(this, "Please use Android 13 device.", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    /**
+     * Shows [message] in a [Snackbar].
+     */
+    private fun showSnackBar(message: String) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        // Set the max lines of SnackBar
+        snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines =
+            10
+        snackBar.show()
+    }
+
 }
